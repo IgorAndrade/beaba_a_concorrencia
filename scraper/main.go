9a46309a7483924b719ace332e36a2cd57de2634
Igor Andrade <igor.andrade@dextra-sw.com>
2020-06-06 16:11:53 -0300

    get title

--- scraper/main.go
+++ scraper/main.go
@@ -1,11 +1,31 @@
 package main
 
+import (
+	"fmt"
+	"regexp"
+)
+
+type Result struct {
+	url   string
+	title string
+}
+
+func (r Result) String() string {
+	return fmt.Sprintf("URL: %s \nTitle: %s", r.url, r.title)
+}
 func main() {
 	urlToProcess := []string{
 		"http://globo.com",
 		"http://google.com",
 		"https://globoesporte.globo.com/",
-		"https://uxdesign.cc/learning-to-code-or-sort-of-will-make-you-a-better-product-designer-e76165bdfc2d",
 	}
 	println(urlToProcess)
+}
+
+func getTitle(html string) string {
+	r, _ := regexp.Compile("<title>(.*?)<\\/title>")
+	if title := r.FindStringSubmatch(html); len(title) > 0 {
+		return r.FindStringSubmatch(html)[1]
+	}
+	return "Sem titulo "
 }
